9e5250ec97104a3171b2607b59479e4436e058a9
odilovovo <@odlovo2@gamil.com>
2026-01-16 14:13:38 +0500

    Sign Up and Log In

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Login from "./Sign up/login/login"
+import Signup from "./Sign up/sign up/sign"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+      <Login />
+      <Signup />
     </>
   )
 }
